bb48772b106eb340752aa0af418933ff19a3cd63
Neil DSouza <nxd_in@yahoo.com>
2018-07-23 00:19:17 +0530

    successfully submitted to hacker rank

--- 2_euler.cpp
+++ 2_euler.cpp
@@ -25,19 +25,19 @@ using namespace std;
 
 #include "big_int.h"
 
-BigInt compute_even_fibonacci_less_than_n(long n, map<BigInt, BigInt> & map_sums) {
+void compute_even_fibonacci_less_than_n(long n, map<BigInt, BigInt> & map_sums) {
 	// cout << "ENTER " << __PRETTY_FUNCTION__ << endl;
 	BigInt bi1(1);
 	BigInt bi2(2);
 	BigInt limit(n);
 	long l1 = 1, l2 = 2;
-	if (n == 1 || n == 2) {
-		return 0;
-	} 
+	//if (n == 1 || n == 2) {
+	//	return 0;
+	//} 
 
 	//long sum = 2;
 	BigInt sum(2);
-	map_sums[sum] = sum;
+	map_sums[BigInt(2)] = BigInt(2);
 	long sum_l(2);
 	do {
 		long next_term_l = l1 + l2;
@@ -45,12 +45,15 @@ BigInt compute_even_fibonacci_less_than_n(long n, map<BigInt, BigInt> & map_sums
 		l1 = l2;
 		bi1 = bi2;
 		// cout << "BigInt next_term: " << next_term << endl;
-		if (next_term_l < n && next_term_l % 2 == 0) {
+		// cout << " next_term_l: " << next_term_l << endl;
+		if (next_term_l <= n && next_term_l % 2 == 0) {
+			// cout << "adding to map next_term_l: " << next_term_l 
+			// 	<< " sum_l: " << sum_l << endl;
 			sum = sum.add(next_term);
 			sum_l += next_term_l;
-			if (map_sums.find(sum) == map_sums.end()) {
-				map_sums[sum] = sum;
-			}
+			map_sums[next_term] = sum;
+			// if (map_sums.find(sum) == map_sums.end()) {
+			// }
 		}
 		l2 = next_term_l;
 		bi2 = next_term;
@@ -59,8 +62,14 @@ BigInt compute_even_fibonacci_less_than_n(long n, map<BigInt, BigInt> & map_sums
 	} while (bi2 < limit);
 	// cout << "returning sum_l : " << sum_l << endl;
 	// cout << "returning sum : " << sum << endl;
-	map_sums[sum] = sum;
-	return sum;
+	//map_sums[sum] = sum;
+	// return sum;
+	// cout << "START Map List" << endl;
+	// for (map<BigInt, BigInt>::iterator it = map_sums.begin();
+	// 		it != map_sums.end(); ++it) {
+	// 	cout << it->first << " " << it->second << endl;
+	// }
+	// cout << "END Map List" << endl;
 }
 
 
@@ -81,10 +90,12 @@ int main()
 		BigInt bi_n(n);
 		std::map<BigInt, BigInt>::iterator it_low =
 			map_sums.lower_bound(bi_n);
-		// cout << " LOWER_BOUND: "
-		// 	<< it_low->first << " "
-		// 	<< it_low->second <<  endl;
-		cout << it_low->first << endl;
+		 --it_low;
+		 //cout << " LOWER_BOUND for n: " << n << " is: "
+		 //	<< it_low->first << " "
+		 //	<< it_low->second <<  endl;
+		// cout << it_low->first << endl;
+		cout << it_low->second << endl;
 		// std::map<BigInt, BigInt>::iterator it_up =
 		// 	map_sums.upper_bound(bi_n);
 		// cout << " UPPER_BOUND: "
